88ff0a1a42332f0f32da24677eb9e31b537082c2
Denzell <denzell.mgbokwere@gmail.com>
2025-04-28 16:48:36 +0200

    added winrate component

---
+++ src/components/WinRate.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div id="WinRate">
+    </div>
+</template>
+<script  lang="ts">
+
+export default{
+    name: "WinRate",
+    setup(){
+
+        return{
+        }
+    },
+    methods: {
+    },
+    mounted(){
+    }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
