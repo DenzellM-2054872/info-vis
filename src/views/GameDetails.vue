cac40d2d678bb42eaa8c19206c99e402e81b902c
DenzellM-2054872 <denzell.mgbokwere@student.uhasselt.be>
2025-04-29 11:29:55 +0200

    more lost changes

--- src/views/GameDetails.vue
+++ src/views/GameDetails.vue
@@ -17,5 +17,11 @@ import WinRate from '@/components/WinRate.vue';
 
 
 <style lang="scss" scoped>
-
+.content{
+    display: flex;
+    flex-direction: column;
+    max-width: inherit;
+    width: 1280px;
+    align-items: center;
+}   
 </style>
